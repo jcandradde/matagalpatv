2edeb55ba6a82dd60c6c92874d24c2dcc990fbdc
jose-luis-orozco <30738329+jose-luis-orozco@users.noreply.github.com>
2018-11-27 14:07:17 -0600

    modif pdf_empleado_resources

--- resources/views/empleados/pdf.blade.php
+++ resources/views/empleados/pdf.blade.php
@@ -8,9 +8,15 @@
             <tr>
                 <th>ID</th>
                 <th>Primer Nombre</th>
+                <th>Segundo Nombre</th>
                 <th>Primer Apellido</th>
+                <th>Segundo Apellido</th>
                 <th>Cedula</th>
                 <th>Fecha de Nacimiento</th>
+                <th>Telefono</th>
+                <th>Correo</th>
+                <th>Sexo</th>
+                <th>Num INSS</th>
             </tr>
         </thead>
         <tbody>
@@ -18,9 +24,16 @@
                 <tr>
                     <td>{{$empleado->id}}</td>
                     <td>{{$empleado->p_nombre}}</td>
+                    <td>{{$empleado->s_nombre}}</td>
                     <td>{{$empleado->p_apellido}}</td>
+                    <td>{{$empleado->s_apellido}}</td>
                     <td>{{$empleado->cedula}}</td>
                     <td>{{$empleado->fecha_nacimiento}}</td>
+                    <td>{{$empleado->telefono}}</td>
+                    <td>{{$empleado->correo_e}}</td>
+                    <td>{{$empleado->sexo}}</td>
+                    <td>{{$empleado->num_inss}}</td>
+
                 </tr>
             @endforeach
         </tbody>
